499b8da0b0dc30eb4ffbd0b90a691859c80463bc
rishanzihad <138320123+rishanzihad@users.noreply.github.com>
2023-10-06 07:56:04 -0700

    first Comit

---
+++ src/Firebase/firebase.config.js
@@ -0,0 +1,15 @@
+
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBvb9aT4_0l2JxK19u3dafyrXo0DYQxEfo",
+  authDomain: "assingment-nine.firebaseapp.com",
+  projectId: "assingment-nine",
+  storageBucket: "assingment-nine.appspot.com",
+  messagingSenderId: "491276010940",
+  appId: "1:491276010940:web:ed2203a81f6f3736e897b9"
+};
+
+
+const app = initializeApp(firebaseConfig);
+export default app
